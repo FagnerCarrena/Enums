ff941de3bf00585462454269ff7145d9213c70f6
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-12-09 19:58:43 -0300

    first commit

---
+++ src/Nivel.java
@@ -0,0 +1,30 @@
+public enum Nivel {
+
+        SEM_ACESSO,
+        ACESSO_BASICO,
+        ACESSO_AVANCADO
+    }
+
+
+public class Curso {
+    String titulo;
+    String descricao;
+    double mensalidade;
+    int duracaoMeses;
+    Turno turno;
+    NivelCurso nivel;
+    StatusCurso status;
+
+    public Curso(String titulo, String descricao, double mensalidade, int duracaoMeses, Turno turno, NivelCurso nivel,
+                 StatusCurso status) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.mensalidade = mensalidade;
+        this.duracaoMeses = duracaoMeses;
+        this.turno = turno;
+        this.nivel = nivel;
+        this.status = status;
+    }
+
+}
+
